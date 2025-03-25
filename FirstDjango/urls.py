14b8841fa50a6a57d865eecd48530b84852cf734
Anton Troickij <3119892@rambler.ru>
2025-03-25 10:33:56 +0300

    Done. M1:task6

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -5,4 +5,6 @@ from MainApp import views
 urlpatterns = [
     path('', views.home),
     path('about', views.about),
+    path('item/<int:item_id>', views.getitem),
+    path('items', views.getitems)
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -11,6 +11,15 @@ author = {
     "email": "vasya@mail.ru"
 }
 
+items = [
+    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+    {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+    {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+    {"id": 7, "name": "Картофель фри" ,"quantity":0},
+    {"id": 8, "name": "Кепка" ,"quantity":124},
+]
+
+
 def home(request):
     text = """
     <h1>"Изучаем django"</h1>
@@ -28,7 +37,21 @@ def about(request):
     """
     return HttpResponse(text)
 
-# items/1
-# items/2
-# ...
-# items/7
+def getitem(request, item_id):
+    """ По указанному id возвращаем имя элемента"""
+    for item in items:
+        if item['id'] == item_id:
+            result = f"""
+            <h2> Имя: {item["name"]}<h2>
+            <p> Количество {item['quantity']}</p>
+            <p> <a href="/items"> Назад к списку товаров </a>
+            """
+            return HttpResponse(result)
+    return HttpResponseNotFound(f"Item with id={item_id} not found")
+
+def getitems(request):
+    result = "<h1> Список товаров <h1><ol>"
+    for item in items:
+        result += f""" <li><a href="/item/{item['id']}"> {item['name']} </li> """
+    result += "</ol>"
+    return HttpResponse(result)
