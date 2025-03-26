aca2df1f3209904256d8b88e9f1ece858a2925be
Anton Troickij <3119892@rambler.ru>
2025-03-26 14:10:39 +0300

    Done. M3:task1-2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,49 +1,51 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from django.http import HttpResponseNotFound
-
-# Create your views here.
-
-
+from MainApp.models import Item
+ 
+ # Create your views here.
+ 
+ 
 items = [
-    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-    {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-    {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
-    {"id": 7, "name": "Картофель фри" ,"quantity":0},
-    {"id": 8, "name": "Кепка" ,"quantity":124},
-]
-
-
+     {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+     {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+     {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+     {"id": 7, "name": "Картофель фри" ,"quantity":0},
+     {"id": 8, "name": "Кепка" ,"quantity":124},
+ ]
+ 
+ 
 def home(request):
-    context = {
-        "name": "Петров Иван Николаевич",
-        "email": "my_mail@mail.ru"
-    }
-    return render(request, "index.html", context)
-
+     context = {
+         "name": "Петров Иван Николаевич",
+         "email": "my_mail@mail.ru"
+     }
+     return render(request, "index.html", context)
+ 
 def about(request):
-    author = {
-    "name": "Иван",
-    "middle_name": "Петрович",
-    "last_name": "Иванов",
-    "phone": "8-923-600-01-02",
-    "email": "vasya@mail.ru"
-    }
-    return render(request, "about.html", {"author": author})
-
-
+     author = {
+     "name": "Иван",
+     "middle_name": "Петрович",
+     "last_name": "Иванов",
+     "phone": "8-923-600-01-02",
+     "email": "vasya@mail.ru"
+     }
+     return render(request, "about.html", {"author": author})
+ 
+ 
 def get_item(request, item_id: int):
-    item = next((item for item in items if item['id'] == item_id), None)
-
-    if item is not None:
-        context = {
-            "item": item
-        }
-        return render(request, "item_page.html", context)
-    return HttpResponseNotFound(f"Item with id={item_id} not found")
-
-
+     item = next((item for item in items if item['id'] == item_id), None)
+ 
+     if item is not None:
+         context = {
+             "item": item
+         }
+         return render(request, "item_page.html", context)
+     return HttpResponseNotFound(f"Item with id={item_id} not found")
+ 
+ 
 def get_items(request):
+    items = Item.objects.all()
     context = {
         "items": items
     }
